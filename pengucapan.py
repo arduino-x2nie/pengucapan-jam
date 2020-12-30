20c5bedb0a206f513308319a06cb8fb58aaf3765
x2nie <x2nie@yahoo.com>
2020-12-31 00:13:44 +0700

    setengah dua belas!

--- pengucapan.py
+++ pengucapan.py
@@ -13,5 +13,99 @@ angka = {
     11: 'sebelas',
     12: 'dua belas',
 }
-def ucapkan(jam,menit):
-    return [angka[jam], angka[menit]]
+
+
+def eja(n):
+    lut1 = ['nol', 'satu', 'dua', 'tiga', 'empat',
+            'lima', 'enam', 'tujuh', 'delapan', 'sembilan',
+            'sepuluh', 'sebelas', 'dua belas', 'tiga belas',
+            'empat belas', 'lima belas', 'enam belas',
+            'tujuh belas', 'delapan belas', 'sembilan belas']
+
+    lut20 = ['x', 'x', 'dua puluh', 'tiga puluh', 'empat puluh',
+             'lima puluh', 'enam puluh', 'tujuh puluh', 'delapan puluh',
+             'sembilan puluh']
+
+    triples = ['x', 'ribu', 'juta', 'miliar',
+               'biliar', 'quadrillion', 'quintillion',
+               'sextillion', 'septillion', 'octillion',
+               'nonillion', 'decillion']
+
+    words = []
+    shift = 0
+    hasNumber = False
+    insertpos = -1
+    while n > 0:
+        ones = n % 10
+        shiftmod = shift % 3
+        shiftdiv = shift / 3
+        if shiftdiv > 0 and shiftmod == 0:
+            insertpos = len(words)
+            hasNumber = False
+        if shiftmod == 2 and ones > 0:
+            words.append('ratus')
+            words.append(lut1[ones])
+            hasNumber = True
+        if shiftmod == 1 and ones > 0:
+            words.append(lut20[ones])
+            hasNumber = True
+        if shiftmod == 0:
+            tens = n % 100
+            if 0 < tens < 20:
+                words.append(lut1[tens])
+                shift += 1
+                n = n / 10
+                hasNumber = True
+            elif ones > 0:
+                words.append(lut1[ones])
+                hasNumber = True
+        if hasNumber and insertpos >= 0:
+            words.insert(insertpos, triples[shiftdiv])
+            insertpos = -1
+            hasNumber = False
+        n = n / 10
+        shift += 1
+        # oldones = ones
+    words.reverse()
+    text = ' '.join(words)
+    result = text.split(' ')
+    return result
+    # return words if words else ['nol']
+
+
+def mid(n):
+    "around `n` +/- 1"
+    return [n-1, n, n+1]
+
+
+def ucapkan(h, m):
+    # jam = angka.get(h)
+    # menit = angka.get(m)
+    jam = eja(h)
+    menit = eja(m)
+    waktu = []
+    if m in mid(0) + [59]:
+        waktu = [jam, 'tepat']
+    elif m in mid(15):
+        waktu = [jam, 'seperempat']
+    elif m in mid(30):
+        jam = angka.get(h+1)
+        waktu = ['setengah', jam]
+    elif m in mid(45):
+        jam = angka.get(h+1)
+        waktu = [jam, 'kurang', 'seperempat']
+    else:
+        waktu = [jam, menit]
+    return flattened(waktu)
+
+
+def flattened(arr):
+    words = []
+    for r in arr:
+        if isinstance(r, list):
+            words += r
+        else:
+            words.append(r)
+    text = ' '.join(words)
+    result = text.split(' ')
+    return result
--- test_pengucapan.py
+++ test_pengucapan.py
@@ -1,5 +1,5 @@
 import unittest
-from pengucapan import ucapkan
+from pengucapan import ucapkan, eja
 
 tepat = 'tepat'
 satu = 'satu'
@@ -13,13 +13,36 @@ delapan = 'delapan'
 sembilan = 'sembilan'
 sepuluh = 'sepuluh'
 sebelas = 'sebelas'
-duabelas = 'dua belas'
+# duabelas = 'dua belas'
+belas = 'belas'
+puluh = 'puluh'
+kurang = 'kurang'
+lebih = 'lebih'
+seperempat = 'seperempat'
+setengah = 'setengah'
 
-class TestDict(unittest.TestCase):
+
+class TestAngka(unittest.TestCase):
+    def test_jari(self):
+        self.assertEqual(eja(2), [dua])
+        self.assertEqual(eja(20), [dua, puluh])
+        self.assertEqual(eja(22), [dua, puluh, dua])
+        self.assertEqual(eja(12), [dua, belas])
+
+
+class TestJam(unittest.TestCase):
 
     def assertFewEqual(self, a, b):
         fewa = a[:len(b)]
         return self.assertEqual(fewa, b)
-    
+
     def test_jam_menit(self):
-        self.assertEqual(ucapkan(1,0), [satu,tepat])
+        self.assertFewEqual(ucapkan(1, 0), [satu, tepat])
+        self.assertFewEqual(ucapkan(11, 0), [sebelas, tepat])
+        self.assertFewEqual(ucapkan(12, 0), [dua, belas, tepat])
+
+    def test_perempat(self):
+        self.assertFewEqual(ucapkan(1, 15), [satu, seperempat])
+        self.assertFewEqual(ucapkan(11, 15), [sebelas, seperempat])
+        self.assertFewEqual(ucapkan(11, 30), [setengah, dua, belas])
+        self.assertFewEqual(ucapkan(11, 45), [dua, belas, kurang, seperempat])
